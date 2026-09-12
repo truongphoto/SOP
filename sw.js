710a73930be64f33b26e75e9d8e6ed3b3a464c26
truongphoto <154951060+truongphoto@users.noreply.github.com>
2026-09-12 19:28:14 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const C='sop-editor-v1';const A=['./','./index.html','./manifest.json','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='sop-editor-v1-2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>{if(new URL(e.request.url).origin===location.origin)e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
